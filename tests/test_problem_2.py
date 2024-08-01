a87e59ca509f0e46680ef70227452d3252dad113
Angelica <Angelica137@users.noreply.github.com>
2024-08-01 19:13:34 +0100

    add test for missing value

--- tests/test_problem_2.py
+++ tests/test_problem_2.py
@@ -5,3 +5,9 @@ def test_rotated_array_search_6():
     input = [6, 7, 8, 9, 10, 1, 2, 3, 4]
     n = 6
     assert rotated_array_search(input, n) == 6
+
+
+def test_rotated_array_search_6():
+    input = [6, 7, 8, 9, 10, 1, 2, 3, 4]
+    n = 33
+    assert rotated_array_search(input, n) == -1
